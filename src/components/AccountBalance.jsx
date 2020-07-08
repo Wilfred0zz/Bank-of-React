739ea0527bd39767383c17c535a1abd4554dcb35
Jing Wen <wenroujing@hotmail.com>
2020-07-08 18:53:36 -0400

    heading animation completed close #4

--- src/components/AccountBalance.jsx
+++ src/components/AccountBalance.jsx
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class AccountBalance extends Component {
-    render() {
-        return (
-            <div>
-                Balance: {this.props.accountBalance.toFixed(2)}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="second-heading">
+        Balance: {this.props.accountBalance.toFixed(2)}
+      </div>
+    );
+  }
 }
 
 export default AccountBalance;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
-import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import AccountBalance from "./AccountBalance";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-    render() {
+  render() {
     return (
-        <div>
-            <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-            <h1>Bank of React</h1>
-            <br/>
-            <Link to ="/LogIn">Log in</Link>
-            <br/>
-            <Link to="/userProfile">User Profile</Link>
-            <br/>
-            <Link to="/Debits">Debits</Link>
-            <br/>
-            <Link to="/Credits">Credits</Link>
-            <AccountBalance accountBalance={this.props.accountBalance}/>
-        </div>
-        );
-    }
-} 
+      <div className="main-content">
+        <h1 className="heading">
+          <span>B</span>
+          <span>a</span>
+          <span>n</span>
+          <span>k</span>
+          <span>&nbsp;</span>
+          <span>o</span>
+          <span>f</span>
+          <span>&nbsp;</span>
+          <span>R</span>
+          <span>e</span>
+          <span>a</span>
+          <span>c</span>
+          <span>t</span>
+        </h1>
+        <br />
+        <Link className="user-nav" to="/LogIn">
+          Log in
+        </Link>
+        <br />
+        <Link className="user-nav" to="/userProfile">
+          User Profile
+        </Link>
+        <br />
+        <Link className="user-nav" to="/Debits">
+          Debits
+        </Link>
+        <br />
+        <Link className="user-nav" to="/Credits">
+          Credits
+        </Link>
+        <AccountBalance accountBalance={this.props.accountBalance} />
+      </div>
+    );
+  }
+}
 export default Home;
